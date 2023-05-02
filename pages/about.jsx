97ab92c0254bb5f6da23316389be9b6fa9af2fe7
dianazepeda3 <90267006+dianazepeda3@users.noreply.github.com>
2023-05-01 22:45:48 -0600

    Rutas dinamicas
    Proyecto de curso de Next, se ha hecho hasta rutas dinamicas

---
+++ pages/about.jsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+
+export default function about() {
+  return (
+    <Layout
+      title="Contact | next.js"
+      description="descripcion"
+    >
+      <div>
+          <h1>About</h1>
+      </div>
+    </Layout>
+  )
+}
---
+++ pages/blog/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+
+export default function contact() {
+  return (
+    <Layout
+      title="Contact | next.js"
+      description="descripcion"
+    >
+        <h1>Contact</h1>
+    </Layout>
+  )
+}
